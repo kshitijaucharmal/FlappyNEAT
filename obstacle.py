e733d031be640a70df601f856f12becdb7652e1a
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-02-22 14:16:01 +0530

    Obstacle

--- obstacle.py
+++ obstacle.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import sys
 
 width = 400
 height = 400
@@ -9,17 +10,17 @@ screen = pygame.display.set_mode((width, height))
 FPS = 60
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, screen):
         super(Floor, self).__init__()
+        self.surface = screen
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.rect = pygame.draw.rect(screen,(255,255,255), pygame.Rect(self.x, y, self.w, self.h))
+        self.rect = pygame.draw.rect(self.surface,(255,255,255), pygame.Rect(self.x, self.y, self.w, self.h))
 
 class Obstacle(pygame.sprite.Sprite):
-
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen):
         self.surface = screen
         self.x = x
         self.y = y
@@ -36,35 +37,32 @@ class Obstacle(pygame.sprite.Sprite):
         y=Shape[random.randint(0,len(Shape)-1)]
         return y
 
-    def draw(self, colour, shape):
+    def draw(self, colour, shape, screen):
         self.pos = (self.x, self.y)
         if shape == 'rectangle':
-            pygame.draw.rect(self.surface, colour, [400, 400, 100, 50], width=10)
+            pygame.draw.rect(screen, colour, [400, 400, 100, 50], width=10)
         elif shape == 'line':
-            pygame.draw.line(self.surface, colour, (0,1250))
-        else:
-            pygame.draw.circle(self.surface, colour, self.pos, self.r)
+            pygame.draw.line(screen, colour, (0,0), (400, 400))
 
 
 def main(screen):
     clock = pygame.time.Clock()  # for speed
 
-    obstacle = Obstacle(250, 1200)
+    obstacle = Obstacle(250, 200, screen)
     colour = obstacle.obs_colour()
     shape = obstacle.obs_shape()
 
-    while True:
+    run = True
+    while run:
         screen.fill((0, 0, 0))
-        clock.tick(60)
+        obstacle.draw(colour, shape, screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-
-        obstacle.draw(colour, shape)
-
+                run = False
 
         pygame.display.update()
+        clock.tick(FPS)
 
 if __name__ == "__main__":
     main(screen)
