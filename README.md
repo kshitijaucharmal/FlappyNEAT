2d2240272f9217f3d13eb15803bc0342797192e1
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-03-04 08:23:58 +0530

    Neural Network can be shown anywhere

---
+++ README.md
@@ -0,0 +1,9 @@
+# INC-Project
+
+## Premise
+This is made for the InC (Impetus and Concepts) competition taking place in PICT.
+The project is simple, an AI will play the simple game of Flappy Bird by using the
+famous NEAT Algorithm.
+
+## Implementation
+The game files are in the root of the directory structure while the AI is in a folder called 'neat'
--- __main__.py
+++ __main__.py
@@ -1,21 +1,79 @@
+# libraries used
 import pygame
+import random
 
-WIDTH = 600
-HEIGHT = 600
+# Files
+from neat.genome import Genome
+from neat.geneh import GeneHistory
+from neat.population import Population
+
+WIDTH = 400
+HEIGHT = 400
+NNWIDTH = WIDTH/2
+NNHEIGHT = HEIGHT/2
 FPS = 60
+BGCOLOR = (11, 11, 11)
 
 pygame.display.init()
 main_screen = pygame.display.set_mode((WIDTH, HEIGHT))
+nn = pygame.Surface((NNWIDTH, NNHEIGHT), pygame.SRCALPHA, 32)
+nn.convert_alpha()
 pygame.display.set_caption("Flappy Bird AI")
 clock = pygame.time.Clock()
+
+population = Population(100, 6, 1)
+sample_inputs = [(random.random()) for a in range(population.n_inputs)]
+
+def keydown_event(brain, event):
+    # Set ctr to show best members of population
+    show = False
+    if event.key == pygame.K_n:
+        population.next()
+    if event.key == pygame.K_b:
+        population.prev()
+    
+    if event.key == pygame.K_c:
+        brain.add_gene()
+    if event.key == pygame.K_a:
+        brain.add_node()
+    if event.key == pygame.K_m:
+        brain.mutate()
+    if event.key == pygame.K_o:
+        print(brain.get_outputs(sample_inputs))
+
+    if event.key == pygame.K_s:
+        # Divide population in species
+        population.speciate()
+        # Evaluate each species
+        for s in range(len(population.species)):
+            population.species[s].evaluate()
+        show = False
+
+    if show:
+        print(brain.get_outputs(sample_inputs))
+        print(brain)
 
 def mainloop():
     run = True
     while run:
-        main_screen.fill((11, 11, 11))
+        # Fill with background color
+        main_screen.fill(BGCOLOR)
+        nn.fill(BGCOLOR)
+
+        # Show the best one
+        population.best.show(nn)
+
+        # Event Handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                keydown_event(population.best, event)
+
+        # Draw neural network
+        main_screen.blit(nn, (WIDTH-NNWIDTH, 0))
+
+        # Updating
         pygame.display.update()
         clock.tick(FPS)
     pass
