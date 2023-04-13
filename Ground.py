30f0c8a3a46c6b16bc6f5d5eb792e9a2cac10196
alisha971 <119912853+alisha971@users.noreply.github.com>
2023-04-13 12:29:27 +0530

    Add files via upload

---
+++ Ground.py
@@ -0,0 +1,20 @@
+import pygame
+from sys import exit
+import random
+ground_image = pygame.image.load("INC/Flappy bird 2/assets/ground.png")
+scroll_speed = 1
+win_height = 720
+win_width = 551
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self) #initialising the base class
+        self.image = ground_image
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+    def update(self):
+        # Move Ground
+        self.rect.x -= scroll_speed
+        if self.rect.x <= -win_width:
+            self.kill()
